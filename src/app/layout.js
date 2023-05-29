c1d2142b256459dd9a73ca3b808439c841e508e2
shaheen2013 <coredeveloper.2013@gmail.com>
2023-05-29 10:39:18 +0600

    working on

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+import '@/css/style.css';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
---
+++ src/pages/test/index.js
@@ -0,0 +1,5 @@
+const Test = () => {
+    return "Hello";
+}
+
+export default Test
